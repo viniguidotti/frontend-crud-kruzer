61856886d7fba2d5ada5f95eb59236243cc7a976
Vini Moraes <57679672+viniguidotti@users.noreply.github.com>
2020-06-01 04:36:26 -0300

    some alterations

--- src/app/components/users/user-read/user-read.component.ts
+++ src/app/components/users/user-read/user-read.component.ts
@@ -2,6 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { UserService } from './../user.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+@Pipe({
+  name: 'dateFormat'
+})
+export class DateFormatPipe extends DatePipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    return super.transform(value, Constants.DATE_FMT);
+  }
+}
+
+export class Constants {
+  static readonly DATE_FMT = 'dd/MMM/yyyy';
+  static readonly DATE_TIME_FMT = `${Constants.DATE_FMT} hh:mm:ss`;
+}
 
 @Component({
   selector: 'app-user-read',
