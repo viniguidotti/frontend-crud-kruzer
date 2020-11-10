00ade37b2435b18abd14cdf0ae730b9742adf137
viniguidotti <vinimoraes321@hotmail.com>
2020-11-10 11:14:59 -0300

    teste url

--- src/app/components/users/user.service.ts
+++ src/app/components/users/user.service.ts
@@ -10,7 +10,7 @@ import { tap, delay, take, map, catchError } from 'rxjs/operators';
 })
 export class UserService {
 
-  baseUrl = "http://3.139.78.183:3000/users/"
+  baseUrl = "https://3.139.78.183:3000/users/"
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
