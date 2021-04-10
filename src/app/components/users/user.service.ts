93ad82b4c77ba0476fa9349eb31d21d8a60dce1f
Vinicius Guidotti Moraes <vinicius@zapway.com.br>
2021-04-10 16:21:10 -0300

    base url API test

--- src/app/components/users/user.service.ts
+++ src/app/components/users/user.service.ts
@@ -10,7 +10,7 @@ import { tap, delay, take, map, catchError } from 'rxjs/operators';
 })
 export class UserService {
 
-  baseUrl = "http://localhost:3000/users/"
+  baseUrl = "https://backend-crud-kruzer.herokuapp.com:3000/users/"
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { 
     
